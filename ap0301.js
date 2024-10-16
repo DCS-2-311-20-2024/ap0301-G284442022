8486058f88280905aca33b8867575f99cff9bcfd
G284442022 <228444@st.takushoku-u.ac.jp>
2024-10-16 14:20:48 +0900

    第3回　第1回
    ロボットの作成
    ロボット増殖
    回転を加える
    ランダム性をつける

--- ap0301.js
+++ ap0301.js
@@ -23,16 +23,33 @@ function init() {
   // 座標軸の設定
   const axes = new THREE.AxesHelper(18);
   scene.add(axes);
+  axes.visible = false;
 
   // ロボットの作成
-  const robot = makeMetalRobot();
-  scene.add(robot);
-
+  const robots = new THREE.Group;
+  for(let x=-4;x<5;x++){
+    for(let z=-4;z<5;z++){
+      let robot;  //変数宣言
+      if(Math.random() < 0.25){
+        robot = makeCBRobot();
+      }else{
+        robot = makeMetalRobot()
+      }
+      robot.position.x = x * 6;
+      robot.position.z = z * 6;
+      robot.position.x = x * 6;
+      robot.position.z = z * 6;
+      robot.rotation.y = Math.atan2(x,z);
+      robots.add(robot);
+    }
+  }
+  scene.add(robots);
   // 光源の設定
-  const light = new THREE.SpotLight();
-  light.position.set(0, 30, 30);
-  scene.add(light);
-  
+  const spotLight = new THREE.SpotLight(0xffffff, 1200);
+  spotLight.position.set(0, 30, 30);
+  spotLight.castShadow=true;
+  scene.add(spotLight);
+
   // カメラの設定
   const camera = new THREE.PerspectiveCamera(
     param.fov, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -53,14 +70,22 @@ function init() {
     camera.lookAt(0, 0, 0);
     camera.updateProjectionMatrix();
     renderer.render(scene, camera);
+    requestAnimationFrame(render);
+    
+    // ロボットの動き
+    robots.children.forEach((robot)=>{
+      robot.rotation.y
+       =(robot.rotation.y + 0.01)%(2*Math.PI);
+      robot.position.y=Math.sin(robot.rotation.y);
+    });
   }
 
   // カメラのコントローラ
   const gui = new GUI();
-  gui.add(param, "fov", 10, 100).onChange(render);
-  gui.add(param, "x", -50, 50).onChange(render);
-  gui.add(param, "y", -50, 50).onChange(render);
-  gui.add(param, "z", -50, 50).onChange(render);
+  gui.add(param, "fov", 10, 100);
+  gui.add(param, "x", -50, 50);
+  gui.add(param, "y", -50, 50);
+  gui.add(param, "z", -50, 50);
   
   // 描画
   render();
